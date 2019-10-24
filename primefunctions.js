df2aa86b512f5012c4d07555582913888dba7b41
cutthroats-unsigned <48347335+cutthroats-unsigned@users.noreply.github.com>
2019-10-23 19:32:39 -0700

    Finished cumulativeSum.

--- primefunctions.js
+++ primefunctions.js
@@ -19,6 +19,24 @@ function primeGen(number) {
   }
   return primeArray;
 }
+function cumulativeSum(number) {
+  const tempArray = [];
+  let tempArray2 = [];
+  let tempNum;
+
+  for (let i = 0; i < number.length; i++) {
+    for (let j = 0; j <= i; j++) {
+      tempArray2.push(number[j]);
+    }
+    tempNum = tempArray2.reduce((a, b) => a + b, 0);
+    tempArray.push(tempNum);
+
+    tempArray2 = [];
+  }
+  return tempArray;
+}
 
 console.log(primeGen(10));
+console.log('---------------------');
+console.log(cumulativeSum([1, 2, 3, 4, 5, 6]));
 console.log('---------------------');
